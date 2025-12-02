eca65b32090026eb40294c73c795d3439ba5b5e4
mdnafijulislam <islam22205101045@diu.edu.bd>
2025-12-02 11:38:56 +0600

    test case

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,21 @@
 </head>
 <body>
     <h2>Welcome to My Portfolio</h2>
+    <?php
+        $name = "John Doe";
+        $age = 30;
+        $skills = ["HTML", "CSS", "JavaScript", "PHP", "Laravel"];
+    ?>
+    <p>My name is <?php echo $name; ?> and I am <?php echo $age; ?> years old.</p>
+    <h3>My Skills:</h3>
+    <ul>
+        <?php foreach ($skills as $skill): ?>
+            <li><?php echo $skill; ?></li>
+        <?php endforeach; ?>
+    </ul> 
+        <?php
+    $name = readline("Enter your name: ");
+    echo "Hello, " . $name . "\n";
+    ?>
 </body>
 </html>
