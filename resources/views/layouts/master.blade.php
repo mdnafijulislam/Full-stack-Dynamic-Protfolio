a56f8745a9b16f69ffe9cb64d5ea368101ea2d64
mdnafijulislam <islam22205101045@diu.edu.bd>
2025-12-07 04:07:39 +0600

    Final Portfolio

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,39 +17,40 @@
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light shadow-sm custom-nav">
   <div class="container">
-    
-    <a class="navbar-brand fw-bold" href="{{ route('home') }}">MyPortfolio</a>
+    <a class="navbar-brand fw-bold text-primary nav-logo" href="{{ route('home') }}">
+      MyPortfolio
+    </a>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="mainNav">
+    <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
         <li class="nav-item">
-          <a class="nav-link" href="{{ route('home') }}">Home</a>
+          <a class="nav-link nav-link-custom" href="{{ route('home') }}">Home</a>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="{{ route('portfolio') }}">Portfolio</a>
+          <a class="nav-link nav-link-custom" href="{{ route('portfolio') }}">Portfolio</a>
         </li>
-        <li class="nav-item">
-  <a class="nav-link" href="{{ route('about') }}">About</a>
-</li>
-
 
         <li class="nav-item">
-          <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+          <a class="nav-link nav-link-custom" href="{{ route('about') }}">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link nav-link-custom" href="{{ route('contact') }}">Contact</a>
         </li>
 
       </ul>
     </div>
-
   </div>
 </nav>
+
 
 
 <main class="py-5">
